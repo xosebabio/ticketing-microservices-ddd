6bd8d73fe522d977ed8c051941dfb6c51decaf10
xbabio <xosebabiosabin@gmail.com>
2026-06-27 15:43:11 +0200

    fix: upgrade to Spring Cloud 2025.1.2 and switch to WebMVC gateway
    - Update Spring Cloud version from 2025.0.0 to 2025.1.2
    - Change from spring-cloud-starter-gateway (reactive) to
      spring-cloud-starter-gateway-server-webmvc (servlet)
    - Remove spring-boot-starter-webflux dependency
    - Update route configuration from webflux to mvc prefix
    - Simplify GatewayApplication (remove exclusion workaround)

--- gateway/src/main/java/org/xbs/gateway/GatewayApplication.java
+++ gateway/src/main/java/org/xbs/gateway/GatewayApplication.java
@@ -2,9 +2,8 @@ package org.xbs.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.simple.SimpleDiscoveryClientAutoConfiguration;
 
-@SpringBootApplication(exclude = {SimpleDiscoveryClientAutoConfiguration.class})
+@SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
